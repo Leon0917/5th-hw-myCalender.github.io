9849998bdf95bfbf8d5a688ba337d42fe7a715d5
Leon Carnagie <leonfross@gmail.com>
2020-05-09 09:07:51 -0700

    updates html, css and js files

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
-    $(".saveBtn") on(click, function {
-        var = $(this).sibling(".info").val();
-        var = $(this).parent().attr("id");
-        console.log(info)
+    $(".saveBtn").on("click", function () {
+        var itemId = $(this).attr("id");
+        console.log(itemId)
+        var blockId = itemId.split("-")[1]
+        var userEntry = $("#input-" + blockId).val();
+        console.log(userEntry,"#input-" + blockId)
+        localStorage.setItem(blockId, userEntry);
     })
 })
 
-localStorage.setItem(time, value);
+
+function updateHour() {
+    $("#presentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
+    var presentHour = moment().hours();
+    for(let i = 9; i <= 17; i++) {
+        $("#input-" + i).val(localStorage.getItem(i))
+        if (i < presentHour){
+            $("#input-" + i).addClass("prevHours")
+        }
+        else if (i === presentHour){
+            $("#input-" + i).addClass("curHours")  
+        }
+        else{
+            $("#input-" + i).addClass("futHours")  
+        }
+    }
+}
+updateHour()
